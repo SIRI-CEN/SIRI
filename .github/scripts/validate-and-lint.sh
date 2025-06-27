7dd3b8e339e3c24a1bfe4012c4a490a709fd2b2f
Stefan de Konink <stefan@konink.de>
2025-06-27 09:30:48 +0200

    validate and lint update

--- .github/scripts/validate-and-lint.sh
+++ .github/scripts/validate-and-lint.sh
@@ -13,7 +13,7 @@ PARSING_ERROR=0
 # Iterate all XML and XSD files
 while IFS= read -r -d $'\0' filename; do
   # Prettify the file using xmllint and save the result to ${filename}.pretty
-  if XMLLINT_INDENT=$'\t' xmllint --encode UTF-8 --pretty 1 "${filename}" >"${filename}.pretty"; then
+  if XMLLINT_INDENT=$'\t' xmllint --encode UTF-8 --format --pretty 1 "${filename}" >"${filename}.pretty"; then
     # Remove lines containing the term "xmlspy" to get rid of advertising this and save the result as ${filename}
     grep -i -v "xmlspy" "${filename}.pretty" >"${filename}"
   else
@@ -22,7 +22,7 @@ while IFS= read -r -d $'\0' filename; do
   fi
   # Remove temp file
   rm "${filename}.pretty"
-done < <(/usr/bin/find . -type f \( -name "*.xsd" -or -name "*.xml" \) -not -path "./git" -print0)
+done < <(/usr/bin/find . -type f \( -name "*.xsd" -or -name "*.xml" \) -not -path "./.git/*" -print0)
 
 if [ ${PARSING_ERROR} -ne 0 ]; then
   exit ${PARSING_ERROR}
